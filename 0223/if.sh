74feab52915db595627f1b0ab7630065a0f41cab
Kwag De Gi <code@skyaperture.com>
2015-02-23 09:35:20 +0900

    nested if

--- 0223/if.sh
+++ 0223/if.sh
@@ -8,3 +8,23 @@ else
 	echo "Not there.."
 fi
 
+choice=4
+
+echo "1. Bash"
+echo "2. Scripting"
+echo "3. Tutorial"
+echo -n "Please choose a word [1,2, or 3]? "
+
+while [ $choice -eq 4 ]; do
+	read choice
+	if [ $choice -eq 1 ]; then
+		echo "You have chosen word: Bash"
+	else
+		if [ $choice -eq 2 ]; then
+			echo "You have chosen word: Tutorial"
+		else
+			echo "Please make a choice between 1-3!"
+			choice=4
+		fi
+	fi
+done
